94ea4b9aba0c34e71cedd9eb2984d2e5f2221e29
Gabriel <macepunky@gmail.com>
2017-10-12 21:52:13 -0300

    12/10

---
+++ asd.js
@@ -0,0 +1,61 @@
+const elName = document.querySelector ('input[name="nombre"]');
+const elLastName = document.querySelector ('input[name="email"]');
+const radios1 = document.querySelector ('#gridRadios1');
+const radios2 = document.querySelector ('#gridRadios2');
+const radios3 = document.querySelector ('#gridRadios3');
+const elChecked = document.querySelector ('input[name="checked"]');
+
+
+function onSubmit()
+        {
+            let valid = true;
+
+
+            //name
+            if(!elName.value)
+            {
+                valid = false;
+                console.log('missing name');
+                
+            }
+            //email
+            if(!elLastName.value)
+            {
+                valid = false;
+                console.log('missing email');
+            }
+
+            //radios
+            if(radios1.checked || radios2.checked || radios3.checked)
+            {
+                valid= true;
+               
+            }else{
+                valid= false;
+                console.log('missing radios');
+            }
+            
+            //checkbox
+            if(!elChecked.checked)
+            {
+                valid = false;
+                console.log('missing checkbox');
+            }
+
+            //validation message
+            if(valid == true)
+            {
+                alert('Success!');
+                console.log (valid);
+                
+                            return valid;
+            }else{
+                alert('Something missing! :C');
+                console.log (valid);
+                
+                            return valid;
+            }
+            
+
+
+        }
